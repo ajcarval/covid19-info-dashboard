646fad0c9a6020bc49be655e9a83a9bb498a4035
ajcarval <35710338+ajcarval@users.noreply.github.com>
2020-03-29 22:44:26 -0400

    * created get_soup method for all the requests * fixed section 1.a to perform some simple transformations * added section 2 on getting Ontario tests * added section 3 on getting Alberta tests * added section backbone of section 4 for BC tests still WIP

--- util/scrapper.py
+++ util/scrapper.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 
 from datetime import datetime
 
@@ -25,17 +26,6 @@ from datetime import datetime
 #       number of cases can be extracted from canada site
 #   b. Tests
 #       scrappe from 'http://www.bccdc.ca/about/news-stories/stories/2020/information-on-novel-coronavirus'
-#4. Quebec
-#   a. cases
-#       number of cases can be extracted from canada site
-#   b. Tests
-#       scrappe from 'https://msss.gouv.qc.ca/professionnels/maladies-infectieuses/coronavirus-2019-ncov/'
-#           In FRENCH
-#4. Nunavut
-#   a. cases
-#       number of cases can be extracted from canada site
-#   b. Tests
-#       scrappe from 'https://www.gov.nu.ca/health/information/covid-19-novel-coronavirus'
 #5. North West Territoris
 #   a. cases
 #       number of cases can be extracted from canada site
@@ -76,8 +66,19 @@ from datetime import datetime
 #       number of cases can be extracted from canada site
 #   b. Tests
 #       scrappe from 'https://www.saskatchewan.ca/government/health-care-administration-and-provider-resources/treatment-procedures-and-guidelines/emerging-public-health-issues/2019-novel-coronavirus/cases-and-risk-of-covid-19-in-saskatchewan'
+#13. Quebec
+#   a. cases
+#       number of cases can be extracted from canada site
+#   b. Tests
+#       scrappe from 'https://msss.gouv.qc.ca/professionnels/maladies-infectieuses/coronavirus-2019-ncov/'
+#           In FRENCH
+#14. Nunavut
+#   a. cases
+#       number of cases can be extracted from canada site
+#   b. Tests
+#       scrappe from 'https://www.gov.nu.ca/health/information/covid-19-novel-coronavirus'
 
-#13. Worldwide data
+#15. Worldwide data
 #   a. confirmed cases
 #       read csv from 'https://data.humdata.org/hxlproxy/api/data-preview.csv?url=https%3A%2F%2Fraw.githubusercontent.com%2FCSSEGISandData%2FCOVID-19%2Fmaster%2Fcsse_covid_19_data%2Fcsse_covid_19_time_series%2Ftime_series_covid19_confirmed_global.csv&filename=time_series_covid19_confirmed_global.csv'
 #   b. deaths
@@ -85,7 +86,7 @@ from datetime import datetime
 #   c. recovered
 #       read csv from 'https://data.humdata.org/hxlproxy/api/data-  preview.csv?url=https%3A%2F%2Fraw.githubusercontent.com%2FCSSEGISandData%2FCOVID-19%2Fmaster%2Fcsse_covid_19_data%2Fcsse_covid_19_time_series%2Ftime_series_covid19_recovered_global.csv&filename=time_series_covid19_recovered_global.csv'
 #
-#14.Tests numbers for select countries
+#16. Tests numbers for select countries
 #   a. Canada
 #       DONE in point one above
 #   b. US
@@ -108,21 +109,41 @@ from datetime import datetime
 #       scrappe from ''
 #   k. Japon
 #       scrappe from ''
+#17. Joins
+#   a. join each province tests with canada on province and date
+#   b. join each countries tests with Worldwide confirmed data on country and date
+#18. Calculations
+#   a. calculate cases and tests per 10K population for countries, Canada Provinces, US States
+#19. to save:
+#   a. canada with cases per day and tests per day for each province
 
-# START
+#Function definitions:
 def writeln(file, string):
     file.write(string + "\n")
 
+def get_soup(URL):
+    page = requests.get(URL)
+    return BeautifulSoup(page.content, 'html.parser')
+
+# START
 report = open("../datasets/.report",'w')
-date_today = datetime.today().strftime('%Y-%m-%d')
+date_today = str(datetime.today().strftime('%Y-%m-%d'))
 writeln(report, date_today + "\n")
 
 #1. Canada
+#1.a. read from website
+writeln(report,"1. Starting Canada csv fetch and operations...")
+canada_df = pd.read_csv('https://health-infobase.canada.ca/src/data/covidLive/covid19.csv')
+canada_df.drop(columns="prnameFR", inplace=True)
+canada_df.drop(columns="numprob", inplace=True)
+canada_df.drop(columns="numtested", inplace=True)
+canada_df['date'] = [ datetime.strptime(x,'%d-%m-%Y').strftime('%Y-%m-%d') for x in canada_df['date'] ]
+#canada_df.to_csv('../datasets/canada.csv')
+writeln(report, "2. Completed Canada csv fetch.")
+
 #1.b. Scrappe for Canada Tests
-writeln(report,"1. Starting Canada test...")
-URL = 'https://www.canada.ca/en/public-health/services/diseases/2019-novel-coronavirus-infection.html'
-page = requests.get(URL)
-soup = BeautifulSoup(page.content, 'html.parser')
+writeln(report,"3. Starting Canada test...")
+soup = get_soup('https://www.canada.ca/en/public-health/services/diseases/2019-novel-coronavirus-infection.html')
 tables = soup.find_all('table', class_='table-condensed')
 ths = tables[0].find_all('th')
 tds = tables[0].find_all('td')
@@ -141,4 +162,63 @@ if(len(tds) != 3):
 with open("../datasets/canada-tests.csv",'a') as ca_tests:
     writeln(ca_tests, date_today + ", " + tds[0].text.replace(",","") + ", " + tds[1].text.replace(",","") + ", " + tds[2].text.replace(",",""))
 
-writeln(report, "2. Completed Canada test.")
+writeln(report, "4. Completed Canada test.")
+
+#2. Ontario
+#   a. cases: total number of cases can be extracted from canada site
+#   b. Tests: scrappe from 'https://www.ontario.ca/page/2019-novel-coronavirus'
+#        really from 'https://api.ontario.ca/api/drupal/page%2F2019-novel-coronavirus?fields=nid,field_body_beta,body'
+writeln(report, "5. Getting Ontario tests...")
+soup = get_soup('https://api.ontario.ca/api/drupal/page%2F2019-novel-coronavirus?fields=nid,field_body_beta,body')
+tables = soup.find_all('table')
+trs = tables[0].find_all('tr')
+# Hardcoding values of attributes with expected ones to compare in case the website changes
+attributes = ['Negative1,','Currently under investigation2,','Confirmed positive3,', 'Resolved4,', 'Deceased,', 'Total number of patients approved for COVID-19 testing to date,']
+record = date_today
+for i in range(0, len(trs)):
+    tds = trs[i].find_all('td')
+    if (tds[0].text != attributes[i]):
+        writeln(report, "ERROR: Ontario tests table on website not what expected")
+        exit()
+    record = records + ", " + tds[1].text.replace(",","")
+
+with open("../datasets/ontario-tests.csv","a") as ontario_tests:
+    writeln(ontario_tests, record)
+
+writeln(report, "6. Completed getting Ontario tests records")
+
+#3. Alberta
+#   a. cases: number of cases can be extracted from canada site
+#   b. Tests: scrappe from 'https://www.alberta.ca/covid-19-alberta-data.aspx'
+writeln(report, "7. Getting Alberta tests...")
+soup = get_soup('https://www.alberta.ca/covid-19-alberta-data.aspx')
+table = soup.find(id='goa-grid25723').find_all(class_='goa-tableouter')[1].find_all('table')[0]
+tr = table.find_all('tr')[0]
+# Hardcoding values of attributes with expected ones to compare in case the website changes
+if ! re.match("Completed tests", tr.find_all('th')[0].text):
+    writeln(report, "ERROR: Alberta tests table on website not what expected")
+    exit()
+record = date_today + ", " + tr.find_all('td')[0].text.replace(",","")
+
+with open("../datasets/alberta-tests.csv","a") as alberta_tests:
+    writeln(alberta_tests, record)
+
+writeln(report, "8. Completed getting Alberta tests records")
+
+#4. BC
+#   a. cases: number of cases can be extracted from canada site
+#   b. Tests: scrappe from 'http://www.bccdc.ca/about/news-stories/stories/2020/information-on-novel-coronavirus'
+writeln(report, "9. Getting BC tests...")
+soup = get_soup('https://www.alberta.ca/covid-19-alberta-data.aspx')
+table = soup.find(id='goa-grid25723').find_all(class_='goa-tableouter')[1].find_all('table')[0]
+tr = table.find_all('tr')[0]
+# Hardcoding values of attributes with expected ones to compare in case the website changes
+if ! re.match("Completed tests", tr.find_all('th')[0].text):
+    writeln(report, "ERROR: Alberta tests table on website not what expected")
+    exit()
+record = date_today + ", " + tr.find_all('td')[0].text.replace(",","")
+
+with open("../datasets/alberta-tests.csv","a") as alberta_tests:
+    writeln(alberta_tests, record)
+
+writeln(report, "10. Completed getting BC tests records")
