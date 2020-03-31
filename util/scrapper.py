3b71f9bddf1213853de024f2ab7dda57bfadd37b
ajcarval <35710338+ajcarval@users.noreply.github.com>
2020-03-31 01:22:25 -0400

    * standardized tests.csv files to all have 2 columns of date and total tests * updated bc-tests.csv file * implemented section 4.b. of scrapper.py to get tests from BC * converted each section on scrapper.py to a function to have a bit more control * created function log(..) for loggin

--- util/scrapper.py
+++ util/scrapper.py
@@ -121,106 +121,170 @@ from datetime import datetime
 def writeln(file, string):
     file.write(string + "\n")
 
+def log(string):
+    if not hasattr(report, "logfile"):
+        report.logfile = open("../datasets/.report",'w')
+        report.errorlog = open("../datasets/.errors",'w')
+    if re.match("^ERROR:", string):
+        writeln(report.errorlog,string)
+    writeln(report.logfile,string)
+
 def get_soup(URL):
     page = requests.get(URL)
     return BeautifulSoup(page.content, 'html.parser')
 
 # START
-report = open("../datasets/.report",'w')
 date_today = str(datetime.today().strftime('%Y-%m-%d'))
-writeln(report, date_today + "\n")
+log(str(datetime.today().strftime('%Y-%m-%d %H:%M:%S')) + "\n")
 
 #1. Canada
 #1.a. read from website
-writeln(report,"1. Starting Canada csv fetch and operations...")
-canada_df = pd.read_csv('https://health-infobase.canada.ca/src/data/covidLive/covid19.csv')
-canada_df.drop(columns="prnameFR", inplace=True)
-canada_df.drop(columns="numprob", inplace=True)
-canada_df.drop(columns="numtested", inplace=True)
-canada_df['date'] = [ datetime.strptime(x,'%d-%m-%Y').strftime('%Y-%m-%d') for x in canada_df['date'] ]
-#canada_df.to_csv('../datasets/canada.csv')
-writeln(report, "2. Completed Canada csv fetch.")
+def get_canada_cases():
+    log("1. Starting Canada csv fetch and operations...")
+    canada_df = pd.read_csv('https://health-infobase.canada.ca/src/data/covidLive/covid19.csv')
+    canada_df.drop(columns="prnameFR", inplace=True)
+    canada_df.drop(columns="numprob", inplace=True)
+    canada_df.drop(columns="numtested", inplace=True)
+    canada_df['date'] = [ datetime.strptime(x,'%d-%m-%Y').strftime('%Y-%m-%d') for x in canada_df['date'] ]
+    #canada_df.to_csv('../datasets/canada.csv')
+    log("2. Completed Canada csv fetch.")
 
 #1.b. Scrappe for Canada Tests
-writeln(report,"3. Starting Canada test...")
-soup = get_soup('https://www.canada.ca/en/public-health/services/diseases/2019-novel-coronavirus-infection.html')
-tables = soup.find_all('table', class_='table-condensed')
-ths = tables[0].find_all('th')
-tds = tables[0].find_all('td')
+def get_canada_tests():
+    log("3. Starting Canada test...")
+    soup = get_soup('https://www.canada.ca/en/public-health/services/diseases/2019-novel-coronavirus-infection.html')
+    tables = soup.find_all('table', class_='table-condensed')
+    ths = tables[0].find_all('th')
+    tds = tables[0].find_all('td')
 
-columns = ["Total number of patients tested in Canada", "Total positive", "Total negative"]
+    columns = ["Total number of patients tested in Canada", "Total positive", "Total negative"]
 
-for i in range(0,len(ths)):
-    if(ths[i].text != columns[i]):
-        writeln(report, "ERROR: web column names don't match canada-tests.csv schema.")
-        exit()
+    for i in range(0,len(ths)):
+        if(ths[i].text != columns[i]):
+            log("ERROR: web column names don't match canada-tests.csv schema.")
+            return
 
-if(len(tds) != 3):
-    writeln(report, "ERROR: web column number is different from canada-tests.csv schema.")
-    exit()
+    if(len(tds) != 3):
+        log("ERROR: web column number is different from canada-tests.csv schema.")
+        return
 
-with open("../datasets/canada-tests.csv",'a') as ca_tests:
-    writeln(ca_tests, date_today + ", " + tds[0].text.replace(",","") + ", " + tds[1].text.replace(",","") + ", " + tds[2].text.replace(",",""))
+    with open("../datasets/canada-tests.csv",'a') as ca_tests:
+        writeln(ca_tests, date_today + ", " + tds[0].text.replace(",","") + ", " + tds[1].text.replace(",","") + ", " + tds[2].text.replace(",",""))
 
-writeln(report, "4. Completed Canada test.")
+    log("4. Completed Canada test.")
 
 #2. Ontario
 #   a. cases: total number of cases can be extracted from canada site
 #   b. Tests: scrappe from 'https://www.ontario.ca/page/2019-novel-coronavirus'
 #        really from 'https://api.ontario.ca/api/drupal/page%2F2019-novel-coronavirus?fields=nid,field_body_beta,body'
-writeln(report, "5. Getting Ontario tests...")
-soup = get_soup('https://api.ontario.ca/api/drupal/page%2F2019-novel-coronavirus?fields=nid,field_body_beta,body')
-tables = soup.find_all('table')
-trs = tables[0].find_all('tr')
-# Hardcoding values of attributes with expected ones to compare in case the website changes
-attributes = ['Negative1,','Currently under investigation2,','Confirmed positive3,', 'Resolved4,', 'Deceased,', 'Total number of patients approved for COVID-19 testing to date,']
-record = date_today
-for i in range(0, len(trs)):
-    tds = trs[i].find_all('td')
-    if (tds[0].text != attributes[i]):
-        writeln(report, "ERROR: Ontario tests table on website not what expected")
-        exit()
-    record = records + ", " + tds[1].text.replace(",","")
+def get_ontario_tests():
+    log("5. Getting Ontario tests...")
+    soup = get_soup('https://api.ontario.ca/api/drupal/page%2F2019-novel-coronavirus?fields=nid,field_body_beta,body')
+    tables = soup.find_all('table')
+    trs = tables[0].find_all('tr')
+    # Hardcoding values of attributes with expected ones to compare in case the website changes
+    attributes = ['Negative1,','Currently under investigation2,','Confirmed positive3,', 'Resolved4,', 'Deceased,', 'Total number of patients approved for COVID-19 testing to date,']
+    record = date_today
+    for i in range(0, len(trs)):
+        tds = trs[i].find_all('td')
+        if (tds[0].text != attributes[i]):
+            log("ERROR: Ontario tests table on website not what expected")
+            exit()
+        record = records + ", " + tds[1].text.replace(",","")
 
-with open("../datasets/ontario-tests.csv","a") as ontario_tests:
-    writeln(ontario_tests, record)
+    with open("../datasets/ontario-tests.csv","a") as ontario_tests:
+        writeln(ontario_tests, record)
 
-writeln(report, "6. Completed getting Ontario tests records")
+    log("6. Completed getting Ontario tests records")
 
 #3. Alberta
 #   a. cases: number of cases can be extracted from canada site
 #   b. Tests: scrappe from 'https://www.alberta.ca/covid-19-alberta-data.aspx'
-writeln(report, "7. Getting Alberta tests...")
-soup = get_soup('https://www.alberta.ca/covid-19-alberta-data.aspx')
-table = soup.find(id='goa-grid25723').find_all(class_='goa-tableouter')[1].find_all('table')[0]
-tr = table.find_all('tr')[0]
-# Hardcoding values of attributes with expected ones to compare in case the website changes
-if ! re.match("Completed tests", tr.find_all('th')[0].text):
-    writeln(report, "ERROR: Alberta tests table on website not what expected")
-    exit()
-record = date_today + ", " + tr.find_all('td')[0].text.replace(",","")
+def get_alberta_tests():
+    log("7. Getting Alberta tests...")
+    soup = get_soup('https://www.alberta.ca/covid-19-alberta-data.aspx')
+    table = soup.find(id='goa-grid25723').find_all(class_='goa-tableouter')[1].find_all('table')[0]
+    tr = table.find_all('tr')[0]
+    # Hardcoding values of attributes with expected ones to compare in case the website changes
+    if ! re.match("Completed tests", tr.find_all('th')[0].text):
+        log("ERROR: Alberta tests table on website not what expected")
+        return
+    record = date_today + ", " + tr.find_all('td')[0].text.replace(",","")
 
-with open("../datasets/alberta-tests.csv","a") as alberta_tests:
-    writeln(alberta_tests, record)
+    with open("../datasets/alberta-tests.csv","a") as alberta_tests:
+        writeln(alberta_tests, record)
 
-writeln(report, "8. Completed getting Alberta tests records")
+    log("8. Completed getting Alberta tests records")
 
 #4. BC
 #   a. cases: number of cases can be extracted from canada site
 #   b. Tests: scrappe from 'http://www.bccdc.ca/about/news-stories/stories/2020/information-on-novel-coronavirus'
-writeln(report, "9. Getting BC tests...")
+def get_bc_tests():
+    log("9. Getting BC tests...")
 
-# WIP: NEED TO FIX COMMENTED OUT CODE TO BE USED FOR BC DATA
-#soup = get_soup('https://www.alberta.ca/covid-19-alberta-data.aspx')
-# table = soup.find(id='goa-grid25723').find_all(class_='goa-tableouter')[1].find_all('table')[0]
-# tr = table.find_all('tr')[0]
-# Hardcoding values of attributes with expected ones to compare in case the website changes
-# if ! re.match("Completed tests", tr.find_all('th')[0].text):
-#     writeln(report, "ERROR: Alberta tests table on website not what expected")
-#     exit()
-# record = date_today + ", " + tr.find_all('td')[0].text.replace(",","")
-#
-# with open("../datasets/alberta-tests.csv","a") as alberta_tests:
-#     writeln(alberta_tests, record)
+    soup = get_soup('http://www.bccdc.ca/about/news-stories/stories/2020/information-on-novel-coronavirus')
+    div = soup.find(id='ctl00_PlaceHolderMain_SubPlaceholder_ctl07__ControlWrapper_RichHtmlField')
 
-writeln(report, "10. Completed getting BC tests records")
+    div2 = div.find_all('div')
+    if len(div2) == 0:
+        log('ERROR: BC tests page not what expected, aborting BC tests get')
+        return
+
+    div3 = div2[0].find_all('div')
+    if len(div3) == 0:
+        log('ERROR: BC tests page not what expected, aborting BC tests get')
+        return
+
+    lis = div3[0].find_all('li')
+    if len(lis) == 0:
+        log('ERROR: BC tests page not what expected, aborting BC tests get')
+        return
+
+# IN TABLE                       # IN WEBSITE
+# -----------------------------------------------------------------
+# cases                          # 970 confirmed cases as of March 30. 
+# recovered                      # 469 recovered in BC
+# deaths                         # 19 deaths in BC
+#                                # Cases by region:
+# Fraser Health                  # 323 in Fraser Health
+# Interior Health                # 94 in Interior Health
+# Island Health                  # 67 in Island Health
+# Northern Health                # 14 in Northern Health
+# Vancouver Coastal Health       # 472 in Vancouver Coastal Health.
+# tests                          # 42,028 tests completed.
+
+    attributes = {}
+
+    try:
+        for li in lis:
+            if re.match("^[0-9,]+[\u00A0 ]confirmed cases", li.text):
+                attributes["cases"] = int(li.text[:min(li.text.find(" "), li.text.find("\u00A0"))].replace(",",""))
+            elif re.match("^[0-9,]+[\u00A0 ]recovered in BC", li.text):
+                attributes["recovered"] = int(li.text[:min(li.text.find(" "), li.text.find("\u00A0"))].replace(",",""))
+            elif re.match("^[0-9,]+[\u00A0 ]deaths in BC", li.text):
+                attributes["deaths"] = int(li.text[:min(li.text.find(" "), li.text.find("\u00A0"))].replace(",",""))
+            elif re.match("^[0-9,]+[\u00A0 ]in Fraser Health", li.text):
+                attributes["Fraser Health"] = int(li.text[:min(li.text.find(" "), li.text.find("\u00A0"))].replace(",",""))
+            elif re.match("^[0-9,]+[\u00A0 ]in Interior Health", li.text):
+                attributes["Interior Health"] = int(li.text[:min(li.text.find(" "), li.text.find("\u00A0"))].replace(",",""))
+            elif re.match("^[0-9,]+[\u00A0 ]in Island Health", li.text):
+                attributes["Island Health"] = int(li.text[:min(li.text.find(" "), li.text.find("\u00A0"))].replace(",",""))
+            elif re.match("^[0-9,]+[\u00A0 ]in Northern Health", li.text):
+                attributes["Northern Health"] = int(li.text[:min(li.text.find(" "), li.text.find("\u00A0"))].replace(",",""))
+            elif re.match("^[0-9,]+[\u00A0 ]in Vancouver Coastal Health", li.text):
+                attributes["Vancouver Coastal Health"] = int(li.text[:min(li.text.find(" "), li.text.find("\u00A0"))].replace(",",""))
+            elif re.match("^[0-9,]+[\u00A0 ]tests completed" in li.text:
+                attributes["tests"] = int(li.text[:min(li.text.find(" "), li.text.find("\u00A0"))].replace(",",""))
+            elif not "Cases by region" in li.text:
+                log("ERROR: BC tests page table not what expected")
+                return
+    except ValueError:
+        log("ERROR: BC tests page doesn't have numbers where expected")
+        return
+
+    record = attributes['cases'] + ", " + attributes['recovered'] + ", " + attributes['deaths'] + ", " + attributes['Fraser Health'] + ", " + attributes['Interior Health'] + ", " + attributes['Island Health'] + ", " + attributes['Northern Health'] + ", " + attributes['Vancouver Coastal Health'] + ", " + attributes['tests']
+
+    with open("../datasets/bc-tests.csv","a") as bc_tests:
+        writeln(bc_tests, record)
+
+    log("10. Completed getting BC tests records")
